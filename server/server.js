d1c9f56012fe3422a103c3f066cc3f082c1e11d6
Shalpreet Kaur <kaurshalpreet@gmail.com>
2020-12-21 20:59:27 -0800

    deploying

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('/build'));
+	app.use(express.static('../build'));
   }
   
 
